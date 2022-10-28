4fa3371442b967c7dacaaf093d05d8ec4038b174
Boyan <92653208+BoyanK95@users.noreply.github.com>
2022-10-28 03:14:19 +0300

    Created ExpenseItem component and added the css for the project

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -1,8 +1,10 @@
+import ExpenseItem from "./components/ExpenseItem";
+
 function App() {
   return (
     <div>
       <h2>Let's get started!!!</h2>
-      <p>This is also visible</p>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }

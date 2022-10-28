1aba4df2bcc6b8ba538a5ce82b2c727776cdc094
Boyan <92653208+BoyanK95@users.noreply.github.com>
2022-10-28 03:20:00 +0300

    Added classes to the ExpenItem component

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -1,10 +1,12 @@
+import './ExpenseItem.css'
+
 function ExpenseItem() {
   return (
-    <div>
+    <div className='expense-item'>
       <div>March 28th 2021</div>
-      <div>
+      <div className='expense-item__description '>
         <h2>Car Insurance</h2>
-      <div>$295.96</div>
+      <div className='expense-item__price'>$295.96</div>
       </div>
     </div>
   );

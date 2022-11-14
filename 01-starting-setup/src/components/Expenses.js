c92e21903e9938f30912dc699a5898923fb5dda3
Boyan <92653208+BoyanK95@users.noreply.github.com>
2022-11-14 16:53:22 +0200

    Created the Card component
    Created the Card component which is a container component for my expenses

--- 01-starting-setup/src/components/Expenses.js
+++ 01-starting-setup/src/components/Expenses.js
@@ -1,8 +1,9 @@
 import ExpenseItem from "./ExpenseItem";
+import Card from "./Card";
 import './Expenses.css'
 
 function Expenses(props) {
-    return <div className="expenses">
+    return <Card className="expenses">
               <ExpenseItem
         title={props.items[0].title}
         amount={props.items[0].amount}
@@ -23,7 +24,7 @@ function Expenses(props) {
         amount={props.items[3].amount}
         date={props.items[3].date}
       />
-    </div>
+    </Card>
 }
 
 export default Expenses;

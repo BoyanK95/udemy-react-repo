c9ee905828f3a32468bf86c3db36d1ce597bad37
Boyan <92653208+BoyanK95@users.noreply.github.com>
2022-11-14 15:53:16 +0200

    Added css style to the Expenses component

--- 01-starting-setup/src/components/Expenses.js
+++ 01-starting-setup/src/components/Expenses.js
@@ -1,7 +1,8 @@
 import ExpenseItem from "./ExpenseItem";
+import './Expenses.css'
 
 function Expenses(props) {
-    return <div>
+    return <div className="expenses">
               <ExpenseItem
         title={props.items[0].title}
         amount={props.items[0].amount}

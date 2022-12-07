1695a9d9a667544208ac2299ed8cdfc3e72d29eb
Boyan <92653208+BoyanK95@users.noreply.github.com>
2022-12-08 01:08:05 +0200

    Created button component

--- Section8PracticeProject/accountAdder/src/components/Users/AddUser.js
+++ Section8PracticeProject/accountAdder/src/components/Users/AddUser.js
@@ -1,5 +1,7 @@
 import React from "react";
+import Button from "../UI/Button";
 import Card from "../UI/Card";
+import classes from './AddUser.module.css'
 
 const AddUser = (props) => {
     const addUserHandler = (ev) => {
@@ -8,13 +10,13 @@ const AddUser = (props) => {
 
 
   return (
-    <Card>
+    <Card className={classes.input}>
     <form onSubmit={addUserHandler}>
       <label htmlFor="username">Username</label>
       <input id="username" type='text'></input>
       <label htmlFor="age">Age (Years)</label>
       <input id="age" type='number'></input>
-      <button type="submit">Add User</button>
+      <Button type="submit">Add User</Button>
     </form>
     </Card>
   );

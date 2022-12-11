6c9a039b4af41bb7eacf5e39badbd50e9dddb91e
Boyan <92653208+BoyanK95@users.noreply.github.com>
2022-12-11 02:18:26 +0200

    Made error form functional

--- Section8PracticeProject/accountAdder/src/components/Users/AddUser.js
+++ Section8PracticeProject/accountAdder/src/components/Users/AddUser.js
@@ -7,13 +7,22 @@ import classes from "./AddUser.module.css";
 const AddUser = (props) => {
   const [enteredUsername, setEnteredUsername] = useState("");
   const [enteredAge, setEnteredAge] = useState("");
+  const [error, setError] = useState()
 
   const addUserHandler = (ev) => {
     ev.preventDefault();
     if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      setError({
+        title: 'Invalid input!',
+        message: 'Please enter a valid name and age (non-empty values)!'
+      })
       return;
     }
     if (Number(enteredAge) < 1) {
+      setError({
+        title: 'Invalid age!',
+        message: 'Please enter a valid age ( > 0)!'
+      })
       return;
     }
     props.onAddUser(enteredUsername, enteredAge);
@@ -28,9 +37,13 @@ const AddUser = (props) => {
     setEnteredAge(ev.target.value);
   };
 
+  const errorHandler = () => {
+    setError(null)
+  }
+
   return (
     <div>
-      <ErrorModal title="Ann error occured!" msg="Something went wrong!" />
+      {error && <ErrorModal title={error.title} msg={error.message} onConfirm={errorHandler}/>}
       <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
           <label htmlFor="username">Username</label>

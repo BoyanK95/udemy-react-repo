2ae95cd3ace29fb51f6543f939fe582a13871002
Boyan <92653208+BoyanK95@users.noreply.github.com>
2022-12-12 04:31:28 +0200

    used useEffect to save user to localStorage

--- Section10/LoginPage/src/App.js
+++ Section10/LoginPage/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
@@ -6,10 +6,19 @@ import MainHeader from './components/MainHeader/MainHeader';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  
+  useEffect(() => {
+    const storedUserInfo = localStorage.getItem('isLoggedIn')
+    
+    if (storedUserInfo === '1') {
+      setIsLoggedIn(true)
+    }
+  }, [])
 
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+    localStorage.setItem('isLoggedIn', '1')
     setIsLoggedIn(true);
   };
 

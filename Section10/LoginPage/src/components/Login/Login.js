b48b43cc4910969260f78a046be87f41f3daaaf7
Boyan <92653208+BoyanK95@users.noreply.github.com>
2022-12-13 19:22:13 +0200

    Created Input component to use for email and password input

--- Section10/LoginPage/src/components/Login/Login.js
+++ Section10/LoginPage/src/components/Login/Login.js
@@ -3,6 +3,7 @@ import Card from "../UI/Card/Card";
 import classes from "./Login.module.css";
 import Button from "../UI/Button/Button";
 import AuthContext from "../../context/auth-context";
+import Input from "../Input/Input";
 
 const emailReducer = (state, action) => {
   if (action.type === "USER_INPUT") {
@@ -43,7 +44,7 @@ const Login = (props) => {
     isValid: null,
   });
 
-  const authCtx = useContext(AuthContext)
+  const authCtx = useContext(AuthContext);
 
   useEffect(() => {
     console.log("EFFECT RUNNING");
@@ -53,19 +54,20 @@ const Login = (props) => {
     };
   }, []);
 
+  const { isValid: emailIsValid } = emailState;
+  const { isValid: passwordIsValid } = passwordState;
+
   useEffect(() => {
     const identifier = setTimeout(() => {
-      console.log('Checking form validity!');
-      setFormIsValid(
-        emailState.isValid && passwordState.isValid
-      )
-    }, 500)
+      console.log("Checking form validity!");
+      setFormIsValid(emailState.isValid && passwordState.isValid);
+    }, 500);
 
     return () => {
-      console.log('CLEANUP');
-      clearTimeout(identifier)
-    }
-  }, [emailState, passwordState])
+      console.log("CLEANUP");
+      clearTimeout(identifier);
+    };
+  }, [emailState, passwordState]);
 
   const emailChangeHandler = (event) => {
     dispachEmail({ type: "USER_INPUT", val: event.target.value });
@@ -74,7 +76,7 @@ const Login = (props) => {
   };
 
   const passwordChangeHandler = (event) => {
-    dispachPassword({type: 'USER_INPUT', val: event.target.value})
+    dispachPassword({ type: "USER_INPUT", val: event.target.value });
 
     // setFormIsValid(emailState.isValid &&  passwordState.isValid);
   };
@@ -84,7 +86,7 @@ const Login = (props) => {
   };
 
   const validatePasswordHandler = () => {
-    dispachPassword({type: "INPUT_INV"})
+    dispachPassword({ type: "INPUT_INV" });
   };
 
   const submitHandler = (event) => {
@@ -95,20 +97,15 @@ const Login = (props) => {
   return (
     <Card className={classes.login}>
       <form onSubmit={submitHandler}>
-        <div
-          className={`${classes.control} ${
-            emailState.isValid === false ? classes.invalid : ""
-          }`}
-        >
-          <label htmlFor="email">E-Mail</label>
-          <input
-            type="email"
-            id="email"
-            value={emailState.value}
-            onChange={emailChangeHandler}
-            onBlur={validateEmailHandler}
-          />
-        </div>
+        <Input
+          id="email"
+          label="E-mail"
+          type="email"
+          isValid={emailIsValid}
+          value={emailState.value}
+          onChange={emailChangeHandler}
+          onBlur={validateEmailHandler}
+        />
         <div
           className={`${classes.control} ${
             passwordState.isValid === false ? classes.invalid : ""

fd8dfda5f6caeb0a0af74baa9ad412a36ed7e1a5
Boyan <92653208+BoyanK95@users.noreply.github.com>
2022-12-13 23:40:45 +0200

    Created Header component

---
+++ Section11/ResturantDeliveryApp/src/components/Layout/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import classes from './Header.module.css'
+import mealsImg from '../../assets/RestorantDinnerImg.jpg'
+
+const Header = props => {
+    return <React.Fragment>
+        <header className={classes.header}>
+            <h1>TradersOfficeMeals</h1>
+            <button>Cart</button>
+        </header>
+        <div className={classes['main-image']}>
+            <img src={mealsImg} alt='A table full of fancy food'/>
+        </div>
+    </React.Fragment>
+}
+
+export default Header

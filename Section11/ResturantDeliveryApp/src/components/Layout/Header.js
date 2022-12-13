3b4bd3c8897cd9647924ee54e34c65bd5c0f6258
Boyan <92653208+BoyanK95@users.noreply.github.com>
2022-12-14 00:04:52 +0200

    Created custom cart-button for the header

--- Section11/ResturantDeliveryApp/src/components/Layout/Header.js
+++ Section11/ResturantDeliveryApp/src/components/Layout/Header.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import classes from './Header.module.css'
 import mealsImg from '../../assets/RestorantDinnerImg.jpg'
+import HeaderCartButton from './HeaderCartButton'
 
 const Header = props => {
     return <React.Fragment>
         <header className={classes.header}>
             <h1>TradersOfficeMeals</h1>
-            <button>Cart</button>
+            <HeaderCartButton />
         </header>
         <div className={classes['main-image']}>
             <img src={mealsImg} alt='A table full of fancy food'/>

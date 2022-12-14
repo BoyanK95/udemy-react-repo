c74a6dacee7fbb916c33a75bd5db122a94dd5fb7
Boyan <92653208+BoyanK95@users.noreply.github.com>
2022-12-14 02:59:07 +0200

    Created MealItemForm and Input components

--- Section11/ResturantDeliveryApp/src/components/Meals/MealItem/MealItem.js
+++ Section11/ResturantDeliveryApp/src/components/Meals/MealItem/MealItem.js
@@ -1,4 +1,5 @@
 import classes from './MealItem.module.css'
+import MealItemForm from './MealItemForm';
 
 
 const MealItem = (props) => {
@@ -12,7 +13,7 @@ const MealItem = (props) => {
         <div className={classes.price}>{price}</div>
       </div>
       <div>
-
+        <MealItemForm />
       </div>
     </li>
   );
---
+++ Section11/ResturantDeliveryApp/src/components/UI/Input.js
@@ -0,0 +1,10 @@
+import classes from './Input.modue.css'
+
+const Input = props => {
+    return <div className={classes.input}>
+        <label htmlFor={props.input.id}>{props.label}</label>
+        <input {...props.input}/>
+    </div>
+}
+
+export default Input

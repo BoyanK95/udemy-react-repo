f98ac6ff094b68667c0274becf8cbc344cb86b9b
Boyan <92653208+BoyanK95@users.noreply.github.com>
2022-12-14 23:11:20 +0200

    updated id for MealItem component

--- Section11/ResturantDeliveryApp/src/components/Meals/AvailableMeals.js
+++ Section11/ResturantDeliveryApp/src/components/Meals/AvailableMeals.js
@@ -4,9 +4,9 @@ import { DUMMY_MEALS } from "./dummy-meals";
 import MealItem from "./MealItem/MealItem";
 
 const AvailableMeal = () => {
-  
   const mealList = DUMMY_MEALS.map((meal) => (
     <MealItem
+      id={meal.id}
       key={meal.id}
       name={meal.name}
       description={meal.description}

9d4730e427b84b7a724fbb42a29b201fee016239
Boyan <92653208+BoyanK95@users.noreply.github.com>
2022-12-14 02:08:15 +0200

    Created Card  and MealItem components

--- Section11/ResturantDeliveryApp/src/components/Meals/AvailableMeals.js
+++ Section11/ResturantDeliveryApp/src/components/Meals/AvailableMeals.js
@@ -1,38 +1,24 @@
+import Card from "../UI/Card";
 import classes from "./AvailableMeals.module.css";
 import { DUMMY_MEALS } from "./dummy-meals";
-// const DUMMY_MEALS = [
-//     {
-//       id: 'm1',
-//       name: 'Sushi',
-//       description: 'Finest fish and veggies',
-//       price: 22.99,
-//     },
-//     {
-//       id: 'm2',
-//       name: 'Schnitzel',
-//       description: 'A german specialty!',
-//       price: 16.5,
-//     },
-//     {
-//       id: 'm3',
-//       name: 'Barbecue Burger',
-//       description: 'American, raw, meaty',
-//       price: 12.99,
-//     },
-//     {
-//       id: 'm4',
-//       name: 'Green Bowl',
-//       description: 'Healthy...and green...',
-//       price: 18.99,
-//     },
-//   ];
+import MealItem from "./MealItem/MealItem";
 
 const AvailableMeal = () => {
-  const mealList = DUMMY_MEALS.map((meal) => <li>{meal.name}</li>);
+  
+  const mealList = DUMMY_MEALS.map((meal) => (
+    <MealItem
+      key={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
 
   return (
     <section className={classes.meals}>
-      <ul>{mealList}</ul>
+      <Card>
+        <ul>{mealList}</ul>
+      </Card>
     </section>
   );
 };
--- Section11/ResturantDeliveryApp/src/components/Meals/MealsSummary.js
+++ Section11/ResturantDeliveryApp/src/components/Meals/MealsSummary.js
@@ -8,7 +8,7 @@ const MealSummary = () => {
         Choose your favorite meal from our broad selection and enjoy a delicious lunch or dinner at home.
       </p>
       <p>
-        All our meals are cooked with high-quality ingredients, just-in-time and
+        All of our meals are cooked with high-quality ingredients, just-in-time and
         of course by experienced chefs!
       </p>
     </section>

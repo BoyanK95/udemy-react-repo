1ebd0a0d68b3fe6d5f4605bd371e10ddad77e4fb
Boyan <92653208+BoyanK95@users.noreply.github.com>
2022-12-20 00:50:49 +0200

    Forwareded the data from the input using ref

--- Section11/ResturantDeliveryApp/src/components/Meals/MealItem/MealItemForm.js
+++ Section11/ResturantDeliveryApp/src/components/Meals/MealItem/MealItemForm.js
@@ -1,10 +1,32 @@
+import { useRef, useState } from "react";
 import Input from "../../UI/Input";
 import classes from "./MealItemForm.module.css";
 
 const MealItemForm = (props) => {
+  const [amountIsValid, setAmountIsValid] = useState(true)
+  const amounInputRef = useRef();
+  const submitHandler = (ev) => {
+    ev.preventDefault();
+
+    const enteredAmount = amounInputRef.current.value;
+    const enteredAmountNumber = +enteredAmount; //Converting it to a Number, same as Number(enteredAmount)
+
+    if (
+      enteredAmount.trim().length === 0 ||
+      enteredAmountNumber < 1 ||
+      enteredAmountNumber > 5
+    ) {
+      setAmountIsValid(false)
+      return;
+    }
+
+    props.onAddToCart(enteredAmountNumber)
+  };
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <Input
+        ref={amounInputRef}
         label="Amount"
         input={{
           id: "amount" + props.id,
@@ -16,6 +38,7 @@ const MealItemForm = (props) => {
         }}
       />
       <button>+ Add</button>
+      {!amountIsValid && <p className={classes.error}>Please enter a valid amount (1-5)</p>}
     </form>
   );
 };

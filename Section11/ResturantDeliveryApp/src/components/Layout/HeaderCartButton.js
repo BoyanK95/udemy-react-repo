585565acb6e3de03a4a105df8f03db9f880ba568
Boyan <92653208+BoyanK95@users.noreply.github.com>
2022-12-23 01:55:20 +0200

    Implemented bump class for the cart to be active only on items change

--- Section11/ResturantDeliveryApp/src/components/Layout/HeaderCartButton.js
+++ Section11/ResturantDeliveryApp/src/components/Layout/HeaderCartButton.js
@@ -1,18 +1,39 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
 import CartContext from "../../store/cart-context";
 
 const HeaderCartButton = (props) => {
-
+  const [btnBump, setBtnBump] = useState(false)
   const cartCtx = useContext(CartContext)
+  
+  const {items} = cartCtx
 
-  const numberOfCartItem = cartCtx.items.reduce((curNumber, item) => {
+  const numberOfCartItem = items.reduce((curNumber, item) => {
     return curNumber + item.amount
   }, 0)
 
+
+  const btnClasses = `${classes.button} ${ btnBump ? classes.bump : ''}`
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return
+    }
+    setBtnBump(true)
+
+    const timer = setTimeout(() => {
+      setBtnBump(false)
+    }, 300);
+
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [items])
+   
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>

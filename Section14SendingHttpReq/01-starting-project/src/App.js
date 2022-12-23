aeae742d950082db04761ebc452bae206c4603f4
Boyan <92653208+BoyanK95@users.noreply.github.com>
2022-12-24 01:43:41 +0200

    Set state for loading and if there are no movies

--- Section14SendingHttpReq/01-starting-project/src/App.js
+++ Section14SendingHttpReq/01-starting-project/src/App.js
@@ -5,8 +5,10 @@ import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false)
 
   async function fetchMoviesHandler() {
+    setIsLoading(true)
     const responce = await fetch("https://swapi.dev/api/films");
     const data = await responce.json();
 
@@ -19,6 +21,7 @@ function App() {
       };
     });
     setMovies(transformedMovies);
+    setIsLoading(false)
   }
 
   return (
@@ -27,7 +30,9 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && <p>Found no movies.</p>}
+        {isLoading && <p>Loading...</p>}
       </section>
     </React.Fragment>
   );

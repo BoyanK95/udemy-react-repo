d3b6b4a8967ec960945de0bd1869ada57a90606d
Boyan <92653208+BoyanK95@users.noreply.github.com>
2022-12-24 03:09:43 +0200

    Created try catch block to handle errors

--- Section14SendingHttpReq/01-starting-project/src/App.js
+++ Section14SendingHttpReq/01-starting-project/src/App.js
@@ -5,23 +5,34 @@ import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
-  const [isLoading, setIsLoading] = useState(false)
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   async function fetchMoviesHandler() {
-    setIsLoading(true)
-    const responce = await fetch("https://swapi.dev/api/films");
-    const data = await responce.json();
+    setIsLoading(true);
+    setError(null);
+    try {
+      const responce = await fetch("https://swapi.dev/api/films");
 
-    const transformedMovies = data.results.map((moviesData) => {
-      return {
-        id: moviesData.episode_id,
-        title: moviesData.title,
-        openingText: moviesData.opening_crawl,
-        releaseDate: moviesData.release_date,
-      };
-    });
-    setMovies(transformedMovies);
-    setIsLoading(false)
+      if (!responce.ok) {
+        throw new Error("Something went wrong!");
+      }
+
+      const data = await responce.json();
+
+      const transformedMovies = data.results.map((moviesData) => {
+        return {
+          id: moviesData.episode_id,
+          title: moviesData.title,
+          openingText: moviesData.opening_crawl,
+          releaseDate: moviesData.release_date,
+        };
+      });
+      setMovies(transformedMovies);
+    } catch (error) {
+      setError(error.message)
+    }
+    setIsLoading(false);
   }
 
   return (
@@ -31,7 +42,8 @@ function App() {
       </section>
       <section>
         {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
-        {!isLoading && movies.length === 0 && <p>Found no movies.</p>}
+        {!isLoading && movies.length === 0 && !error && <p>Found no movies.</p>}
+        {!isLoading && error && <p>{error}</p>}
         {isLoading && <p>Loading...</p>}
       </section>
     </React.Fragment>

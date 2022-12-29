f2304f4c1a68db76221eda3b9abc2ae070b8724a
Boyan <92653208+BoyanK95@users.noreply.github.com>
2022-12-29 04:50:11 +0200

    downloaded resources

--- Section15CustomHooks/04-onwards-to-a-more-realistic-example/src/App.js
+++ Section15CustomHooks/04-onwards-to-a-more-realistic-example/src/App.js
@@ -2,38 +2,13 @@ import React, { useEffect, useState } from 'react';
 
 import Tasks from './components/Tasks/Tasks';
 import NewTask from './components/NewTask/NewTask';
+import useHttp from './hooks/use-http';
 
 function App() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
   const [tasks, setTasks] = useState([]);
+  useHttp({})
 
-  const fetchTasks = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        'https://react-http-6b4a6.firebaseio.com/tasks.json'
-      );
-
-      if (!response.ok) {
-        throw new Error('Request failed!');
-      }
-
-      const data = await response.json();
-
-      const loadedTasks = [];
-
-      for (const taskKey in data) {
-        loadedTasks.push({ id: taskKey, text: data[taskKey].text });
-      }
-
-      setTasks(loadedTasks);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
-    }
-    setIsLoading(false);
-  };
+  
 
   useEffect(() => {
     fetchTasks();

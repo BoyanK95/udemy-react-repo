601e5a323e156ab9087e88ec4cb0f9fe95b3954c
Boyan <92653208+BoyanK95@users.noreply.github.com>
2023-01-05 03:01:43 +0200

    Added increase by 10 component using attacked payload for actions

--- Section18ReduxProject/01-starting-project/src/store/index.js
+++ Section18ReduxProject/01-starting-project/src/store/index.js
@@ -6,6 +6,11 @@ const counterReducer = (state = {counter: 0}, action) => {
             counter: state.counter + 1
         }
     }
+    if (action.type === 'increase') {
+        return {
+            counter: state.counter + action.amount
+        }
+    }
 
     if (action.type === 'decrement') {
         return {

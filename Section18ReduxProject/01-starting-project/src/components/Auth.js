5f6a222a04d9f59b24a8bddd3aa56091c0c4ae88
Boyan <92653208+BoyanK95@users.noreply.github.com>
2023-01-07 18:48:56 +0200

    Implemented login and logout functionality

--- Section18ReduxProject/01-starting-project/src/components/Auth.js
+++ Section18ReduxProject/01-starting-project/src/components/Auth.js
@@ -1,10 +1,20 @@
+import { useDispatch } from 'react-redux';
+import { authActions } from '../store';
 import classes from './Auth.module.css';
 
 const Auth = () => {
+  const dispatch = useDispatch()
+
+  function loginHandler(event) {
+    event.preventDefault()
+
+    dispatch(authActions.login())
+  }
+
   return (
     <main className={classes.auth}>
       <section>
-        <form>
+        <form onSubmit={loginHandler}>
           <div className={classes.control}>
             <label htmlFor='email'>Email</label>
             <input type='email' id='email' />
--- Section18ReduxProject/01-starting-project/src/components/Header.js
+++ Section18ReduxProject/01-starting-project/src/components/Header.js
@@ -1,22 +1,33 @@
-import classes from './Header.module.css';
+import { useSelector, useDispatch } from "react-redux";
+import classes from "./Header.module.css";
+import { authActions } from "../store";
 
 const Header = () => {
+  const isAuth = useSelector((state) => state.auth.isAuthenticated);
+  const dispatch = useDispatch()
+
+  function logoutHandler() {
+    dispatch(authActions.logout())
+  }
+
   return (
     <header className={classes.header}>
       <h1>Redux Auth</h1>
-      <nav>
-        <ul>
-          <li>
-            <a href='/'>My Products</a>
-          </li>
-          <li>
-            <a href='/'>My Sales</a>
-          </li>
-          <li>
-            <button>Logout</button>
-          </li>
-        </ul>
-      </nav>
+      {isAuth && (
+        <nav>
+          <ul>
+            <li>
+              <a href="/">My Products</a>
+            </li>
+            <li>
+              <a href="/">My Sales</a>
+            </li>
+            <li>
+              <button onClick={logoutHandler}>Logout</button>
+            </li>
+          </ul>
+        </nav>
+      )}
     </header>
   );
 };

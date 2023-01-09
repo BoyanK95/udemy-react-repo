aecc0cc6c8534d755a9434890d10b2dcbb978e76
Boyan <92653208+BoyanK95@users.noreply.github.com>
2023-01-09 15:48:15 +0200

    Finished the logic for the cartReducer  which will hold the cart object info

---
+++ Section19AdvanceRedux/01-starting-project/src/store/cart-slice.js
@@ -0,0 +1,40 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState: {
+    items: [],
+    totalQuantity: 0,
+  },
+  reducers: {
+    addItemToCart(state, action) {
+      const newItem = action.payload;
+      const existingItem = state.items.find((item) => item.id === newItem.id);
+      if (!existingItem) {
+        state.items.push({
+          itemId: newItem.id,
+          price: newItem.price,
+          quantity: 1,
+          totalPrice: newItem.price,
+          name: newItem.title
+        });
+      } else {
+        existingItem.quantity++
+        existingItem.totalPrice = existingItem.totalPrice + newItem.price
+      }
+    },
+    removeitemFromCart(state, action) {
+        const id = action.payload
+        const existingItem = state.items.find(item => item.id === id)
+        if (existingItem.quantity === 1) {
+            state.items = state.items.filter(item => item.id !== id)
+        } else {
+            existingItem.quantity--
+            existingItem.totalPrice = existingItem.totalPrice - existingItem.price
+        }
+    },
+  },
+});
+
+export const cartActions = cartSlice.actions
+export default cartSlice

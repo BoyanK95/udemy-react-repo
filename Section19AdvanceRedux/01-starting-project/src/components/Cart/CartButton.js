12d88eeb8f63a40f0902f66feaa08b890bb9006b
Boyan <92653208+BoyanK95@users.noreply.github.com>
2023-01-10 02:05:34 +0200

    Display cartQty using useSelector hook and redux state and finished Add to cart functionality

--- Section19AdvanceRedux/01-starting-project/src/components/Cart/CartButton.js
+++ Section19AdvanceRedux/01-starting-project/src/components/Cart/CartButton.js
@@ -1,9 +1,10 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { uiActions } from '../../store/ui-slice';
 import classes from './CartButton.module.css';
 
 const CartButton = (props) => {
   const dispatch = useDispatch()
+  const cartQty = useSelector(state => state.cart.totalQuantity)
 
   function toggleCartHandler() {
     dispatch(uiActions.toggle())
@@ -12,7 +13,7 @@ const CartButton = (props) => {
   return (
     <button className={classes.button} onClick={toggleCartHandler}>
       <span>My Cart</span>
-      <span className={classes.badge}>1</span>
+      <span className={classes.badge}>{cartQty}</span>
     </button>
   );
 };

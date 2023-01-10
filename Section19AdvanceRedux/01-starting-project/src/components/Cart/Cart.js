dd4d3e369d9d34ae5cf78a5af9d2c72072a76349
Boyan <92653208+BoyanK95@users.noreply.github.com>
2023-01-10 20:38:55 +0200

    Created removeItemHandler and addItemHandler using dispatch hook from react-redux

--- Section19AdvanceRedux/01-starting-project/src/components/Cart/Cart.js
+++ Section19AdvanceRedux/01-starting-project/src/components/Cart/Cart.js
@@ -7,12 +7,13 @@ const Cart = (props) => {
   const cartItems = useSelector((state) => state.cart.items);
   return (
     <Card className={classes.cart}>
-      <h2>Your Shopping Cart</h2>
+      {cartItems.length === 0 ? <h2>Your Shoping Cart is Empty!</h2> : <h2>Your Shopping Cart</h2>}
       <ul>
         {cartItems.map((item) => (
           <CartItem
             key={item.id}
             item={{
+              id: item.id,
               title: item.name,
               quantity: item.quantity,
               total: item.totalPrice,
--- Section19AdvanceRedux/01-starting-project/src/components/Cart/CartItem.js
+++ Section19AdvanceRedux/01-starting-project/src/components/Cart/CartItem.js
@@ -1,7 +1,22 @@
+import { useDispatch } from 'react-redux';
+import { cartActions } from '../../store/cart-slice';
 import classes from './CartItem.module.css';
 
 const CartItem = (props) => {
-  const { title, quantity, total, price } = props.item;
+  const { title, quantity, total, price, id } = props.item;
+  const dispatch = useDispatch()
+
+  const removeItemHandler = () => {
+    dispatch(cartActions.removeitemFromCart(id))
+  }
+
+  const addItemHandler = () => {
+    dispatch(cartActions.addItemToCart({
+      id,
+      title,
+      price
+    }))
+  }
 
   return (
     <li className={classes.item}>
@@ -17,8 +32,8 @@ const CartItem = (props) => {
           x <span>{quantity}</span>
         </div>
         <div className={classes.actions}>
-          <button>-</button>
-          <button>+</button>
+          <button onClick={removeItemHandler}>-</button>
+          <button onClick={addItemHandler}>+</button>
         </div>
       </div>
     </li>
